cdfc22c6620061a6e29e8e5c55cfde6ffd1d887c
aziziega <109194944+aziziega@users.noreply.github.com>
2022-10-30 22:33:06 +0700

    first commit

---
+++ assets/js/text.js
@@ -0,0 +1,35 @@
+// Slogan TYPEWRITER EFFECT
+
+let line = document.getElementById("slogan")
+let txts = ['a coder', 'a gaming player', 'a Front-end Developer', 'also an Student in Amikom Yogyakarta'];
+let speed = 50
+
+async function typewriter(txt) {
+	for (let i = 0; i < txt.length; i++){
+		line.innerHTML += txt[i];
+		await delay(speed)
+	}
+}
+
+async function reverseTypewriter(txt) {
+	for (let i = txt.length; i > 0; i--) {
+		line.innerHTML = line.innerHTML.slice(0, -1) 
+		await delay(speed)
+	}
+}
+
+async function writeLoop() {
+	for (let i = 0; i < txts.length; i++) {
+		await typewriter(txts[i])
+		await delay(1000)
+		await reverseTypewriter(txts[i])
+		await delay(300)
+	}
+	writeLoop()
+}
+
+function delay(ms) {
+	return new Promise((resolve) => { setTimeout(() => { resolve() }, ms) })
+}
+
+writeLoop()
